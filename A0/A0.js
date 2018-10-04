45658941f91806ed183be0f5db374a2935667557
Kakashi <Kakashi@Loki.hfu-open.fu.hs-furtwangen.de>
2018-10-04 16:44:21 +0200

    A0 Anfang

---
+++ A0/A0.js
@@ -0,0 +1,7 @@
+function Name() {
+    var input = prompt("Bitte geben Sie Ihren Namen ein:");
+    var b = String(input);
+    console.log("Name: " + b);
+    console.log("Hallo: " + b);
+}
+//# sourceMappingURL=A0.js.map
---
+++ A0/A0.ts
@@ -0,0 +1,8 @@
+
+function Name() {
+    var input = prompt ("Bitte geben Sie Ihren Namen ein:");
+    var b = String (input);
+            
+    console.log("Name: " + b)
+    console.log("Hallo: " + b);
+    }
