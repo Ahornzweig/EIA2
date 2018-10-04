2e67a09165c858a7e77262c146298ca0ef71eea0
Kakashi <Kakashi@Loki.hfu-open.fu.hs-furtwangen.de>
2018-10-04 18:57:40 +0200

    2

--- A0/A0.js
+++ A0/A0.js
@@ -1,7 +1,17 @@
-function Name() {
+function writeHTML() {
+    var node = document.getElementById("main");
+    var childNodeHTML;
     var input = prompt("Bitte geben Sie Ihren Namen ein:");
     var b = String(input);
+    if (b != null) {
+        childNodeHTML = "";
+        childNodeHTML += "<a>";
+        childNodeHTML += "Hallo: " + b;
+        childNodeHTML += "</a>";
+        node.innerHTML += childNodeHTML;
+    }
     console.log("Name: " + b);
     console.log("Hallo: " + b);
 }
+document.addEventListener('DOMContentLoaded', writeHTML);
 //# sourceMappingURL=A0.js.map
--- A0/A0.ts
+++ A0/A0.ts
@@ -1,8 +1,25 @@
 
-function Name() {
-    var input = prompt ("Bitte geben Sie Ihren Namen ein:");
-    var b = String (input);
-            
+function writeHTML(): void {
+
+    var node: any = document.getElementById("main");
+    var childNodeHTML: string;
+
+    var input: string = prompt("Bitte geben Sie Ihren Namen ein:");
+    var b = String(input);
+
+    if (b != null) {
+
+        childNodeHTML = "";
+        childNodeHTML += "<a>";
+        childNodeHTML += "Hallo: " + b;
+        childNodeHTML += "</a>";
+
+        node.innerHTML += childNodeHTML;
+
+    }
+
     console.log("Name: " + b)
     console.log("Hallo: " + b);
-    }
+}
+
+document.addEventListener('DOMContentLoaded', writeHTML);
