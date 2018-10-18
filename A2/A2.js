9d5181f0cca824cfec3c8098344704f5bbd7d750
Kakashi <Kakashi@Loki.eduroam.hs-furtwangen.de>
2018-10-18 19:46:14 +0200

    2

--- A2/A2.js
+++ A2/A2.js
@@ -1,3 +1,10 @@
+/*Aufgabe: Aufgabe 2
+Name: Sarah L�nnqvist
+Matrikel: 259116
+Datum: 18.10.2018
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
 var A2;
 (function (A2) {
     let k0 = {
--- A2/A2.ts
+++ A2/A2.ts
@@ -1,3 +1,10 @@
+/*Aufgabe: Aufgabe 2
+Name: Sarah Lönnqvist
+Matrikel: 259116
+Datum: 18.10.2018
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
 namespace A2 {
 
     interface Karten {
