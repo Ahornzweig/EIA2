ced276a72937fd1af3c7ec061823dfdd81bdbe57
Sarah <Sarah@LAPTOP-AJIP9J56>
2018-11-16 19:45:31 +0100

    2

--- A4/A4Kunde.js
+++ A4/A4Kunde.js
@@ -1,13 +1,14 @@
 var A4;
 (function (A4) {
     document.addEventListener("DOMContentLoaded", fillFieldset);
+    document.addEventListener("DOMContentLoaded", init);
     function fillFieldset() {
         var node = document.getElementById("fieldset");
         var childNodeHTML;
         //B�ume
         childNodeHTML = "Baum";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='Baume'>";
+        childNodeHTML += "<select name='Select' id='trees'>";
         for (var i = 0; i < A4.treeSpecies.length; i++) {
             childNodeHTML += "<option value='" + A4.treeSpecies[i].name + "'>" + A4.treeSpecies[i].name + "</option>";
         }
@@ -17,14 +18,14 @@ var A4;
         childNodeHTML += "Halterung";
         childNodeHTML += "<br>";
         for (var i = 0; i < A4.holder.length; i++) {
-            childNodeHTML += "<input type='radio' name='Radiogroup' value='radio" + i + "' id='radio" + i + "' />";
+            childNodeHTML += "<input type='radio' name='Radiogroup' value='" + A4.holder[i].name + "'  id='radio" + i + "' />";
             childNodeHTML += "<label for='check" + i + "'>" + A4.holder[i].name + "</label>";
         }
         childNodeHTML += "<br>";
         //Glaskugeln
         childNodeHTML += "Glaskugeln";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='select'>";
+        childNodeHTML += "<select name='Select' id='kugeln'>";
         for (var i = 0; i < A4.christmasBall.length; i++) {
             childNodeHTML += "<option value='" + A4.christmasBall[i].name + "'>" + A4.christmasBall[i].name + "</option>";
         }
@@ -32,7 +33,7 @@ var A4;
         childNodeHTML += "<br>";
         childNodeHTML += "Menge";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='select'>";
+        childNodeHTML += "<select name='Select' id='Menge1'>";
         for (var i = 1; i < 51; i++) {
             childNodeHTML += "<option value='" + i + "'>" + i + "</option>";
         }
@@ -41,7 +42,7 @@ var A4;
         //Lametta
         childNodeHTML += "Lametta";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='select'>";
+        childNodeHTML += "<select name='Select' id='lametten'>";
         for (var i = 0; i < A4.lametta.length; i++) {
             childNodeHTML += "<option value='" + A4.lametta[i].name + "'>" + A4.lametta[i].name + "</option>";
         }
@@ -49,7 +50,7 @@ var A4;
         childNodeHTML += "<br>";
         childNodeHTML += "Laenge in Meter";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='select'>";
+        childNodeHTML += "<select name='Select' id='Menge2'>";
         for (var i = 1; i < 6; i++) {
             childNodeHTML += "<option value='" + i + "'>" + i + "</option>";
         }
@@ -58,7 +59,7 @@ var A4;
         //Kerzen
         childNodeHTML += "Kerzen";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='select'>";
+        childNodeHTML += "<select name='Select' id='candles'>";
         for (var i = 0; i < A4.candleTyp.length; i++) {
             childNodeHTML += "<option value='" + A4.candleTyp[i].name + "'>" + A4.candleTyp[i].name + "</option>";
         }
@@ -66,7 +67,7 @@ var A4;
         childNodeHTML += "<br>";
         childNodeHTML += "Menge";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='select'>";
+        childNodeHTML += "<select name='Select' id='Menge3'>";
         for (var i = 1; i < 21; i++) {
             childNodeHTML += "<option value='" + i + "'>" + i + "</option>";
         }
@@ -75,7 +76,7 @@ var A4;
         //Lieferoptionen
         childNodeHTML += "Lieferoptionen";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='select'>";
+        childNodeHTML += "<select name='Select' id='ship'>";
         for (var i = 0; i < A4.shipment.length; i++) {
             childNodeHTML += "<option value='" + A4.shipment[i].name + "'>" + A4.shipment[i].name + "</option>";
         }
@@ -84,24 +85,113 @@ var A4;
         //Adresse
         childNodeHTML += "Adresse";
         childNodeHTML += "<br>";
-        childNodeHTML += "<input type='text' name='Text' placeholder='enter adress here' required/>";
+        childNodeHTML += "<input id='ad' type='text' name='Text' placeholder='enter adress here' required/>";
         node.innerHTML += childNodeHTML;
     }
-    var fieldset = document.getElementById("fieldset");
-    fieldset.addEventListener("change", handleChange);
+    //Change Listener
+    function init(_event) {
+        var fieldset = document.getElementById("fieldset");
+        fieldset.addEventListener("change", handleChange);
+    }
     function handleChange(_event) {
         var target = _event.target;
-        var checkoutAttribute = target.getAttribute("value");
-        if (target.id == "Baume") {
+        //trees
+        if (target.id == "trees") {
             var node = document.getElementById("tree");
             var childNodeHTML = void 0;
             childNodeHTML = "";
             childNodeHTML += "<a>";
-            childNodeHTML += checkoutAttribute;
+            childNodeHTML += " " + target.value;
             childNodeHTML += "</a>";
-            node.innerHTML += childNodeHTML;
+            node.innerHTML = childNodeHTML;
         }
-        //let checkout: HTMLElement = document.getElementById("div")
+        //halterung
+        if (target.name == "Radiogroup") {
+            var node = document.getElementById("holder");
+            var childNodeHTML = void 0;
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+            node.innerHTML = childNodeHTML;
+        }
+        //balls
+        if (target.id == "kugeln") {
+            var node = document.getElementById("ball");
+            var childNodeHTML = void 0;
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+            node.innerHTML = childNodeHTML;
+        }
+        if (target.id == "Menge1") {
+            var node = document.getElementById("ballMenge");
+            var childNodeHTML = void 0;
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+            node.innerHTML = childNodeHTML;
+        }
+        //Lametta
+        if (target.id == "lametten") {
+            var node = document.getElementById("lametta");
+            var childNodeHTML = void 0;
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+            node.innerHTML = childNodeHTML;
+        }
+        if (target.id == "Menge2") {
+            var node = document.getElementById("lamettaMenge");
+            var childNodeHTML = void 0;
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+            node.innerHTML = childNodeHTML;
+        }
+        //Kerzen
+        if (target.id == "candles") {
+            var node = document.getElementById("candle");
+            var childNodeHTML = void 0;
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+            node.innerHTML = childNodeHTML;
+        }
+        if (target.id == "Menge3") {
+            var node = document.getElementById("candleMenge");
+            var childNodeHTML = void 0;
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+            node.innerHTML = childNodeHTML;
+        }
+        //Lieferoption
+        if (target.id == "ship") {
+            var node = document.getElementById("shipping");
+            var childNodeHTML = void 0;
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+            node.innerHTML = childNodeHTML;
+        }
+        //Adresse
+        if (target.id == "ad") {
+            var node = document.getElementById("adress");
+            var childNodeHTML = void 0;
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+            node.innerHTML = childNodeHTML;
+        }
     }
 })(A4 || (A4 = {}));
 //# sourceMappingURL=A4Kunde.js.map
--- A4/A4Kunde.ts
+++ A4/A4Kunde.ts
@@ -1,5 +1,6 @@
 namespace A4 {
     document.addEventListener("DOMContentLoaded", fillFieldset);
+    document.addEventListener("DOMContentLoaded", init);
 
     function fillFieldset(): void {
         let node: HTMLElement = document.getElementById("fieldset");
@@ -7,7 +8,7 @@ namespace A4 {
         //Bäume
         childNodeHTML = "Baum";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='Baume'>";
+        childNodeHTML += "<select name='Select' id='trees'>";
         for (let i: number = 0; i < treeSpecies.length; i++) {
 
             childNodeHTML += "<option value='" + treeSpecies[i].name + "'>" + treeSpecies[i].name + "</option>";
@@ -19,7 +20,7 @@ namespace A4 {
         childNodeHTML += "Halterung";
         childNodeHTML += "<br>";
         for (let i: number = 0; i < holder.length; i++) {
-            childNodeHTML += "<input type='radio' name='Radiogroup' value='radio" + i + "' id='radio" + i + "' />";
+            childNodeHTML += "<input type='radio' name='Radiogroup' value='" +holder[i].name +"'  id='radio" + i + "' />";
             childNodeHTML += "<label for='check" + i + "'>" + holder[i].name + "</label>";
         }
         childNodeHTML += "<br>";
@@ -27,7 +28,7 @@ namespace A4 {
         //Glaskugeln
         childNodeHTML += "Glaskugeln";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='select'>";
+        childNodeHTML += "<select name='Select' id='kugeln'>";
         for (let i: number = 0; i < christmasBall.length; i++) {
 
             childNodeHTML += "<option value='" + christmasBall[i].name + "'>" + christmasBall[i].name + "</option>";
@@ -37,7 +38,7 @@ namespace A4 {
 
         childNodeHTML += "Menge";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='select'>";
+        childNodeHTML += "<select name='Select' id='Menge1'>";
         for (let i: number = 1; i < 51; i++) {
 
             childNodeHTML += "<option value='" + i + "'>" + i + "</option>";
@@ -48,7 +49,7 @@ namespace A4 {
         //Lametta
         childNodeHTML += "Lametta";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='select'>";
+        childNodeHTML += "<select name='Select' id='lametten'>";
         for (let i: number = 0; i < lametta.length; i++) {
 
             childNodeHTML += "<option value='" + lametta[i].name + "'>" + lametta[i].name + "</option>";
@@ -58,7 +59,7 @@ namespace A4 {
 
         childNodeHTML += "Laenge in Meter";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='select'>";
+        childNodeHTML += "<select name='Select' id='Menge2'>";
         for (let i: number = 1; i < 6; i++) {
 
             childNodeHTML += "<option value='" + i + "'>" + i + "</option>";
@@ -69,7 +70,7 @@ namespace A4 {
         //Kerzen
         childNodeHTML += "Kerzen";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='select'>";
+        childNodeHTML += "<select name='Select' id='candles'>";
         for (let i: number = 0; i < candleTyp.length; i++) {
 
             childNodeHTML += "<option value='" + candleTyp[i].name + "'>" + candleTyp[i].name + "</option>";
@@ -79,7 +80,7 @@ namespace A4 {
 
         childNodeHTML += "Menge";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='select'>";
+        childNodeHTML += "<select name='Select' id='Menge3'>";
         for (let i: number = 1; i < 21; i++) {
 
             childNodeHTML += "<option value='" + i + "'>" + i + "</option>";
@@ -90,7 +91,7 @@ namespace A4 {
         //Lieferoptionen
         childNodeHTML += "Lieferoptionen";
         childNodeHTML += "<br>";
-        childNodeHTML += "<select name='Select' id='select'>";
+        childNodeHTML += "<select name='Select' id='ship'>";
         for (let i: number = 0; i < shipment.length; i++) {
 
             childNodeHTML += "<option value='" + shipment[i].name + "'>" + shipment[i].name + "</option>";
@@ -101,32 +102,150 @@ namespace A4 {
         //Adresse
         childNodeHTML += "Adresse";
         childNodeHTML += "<br>";
-        childNodeHTML += "<input type='text' name='Text' placeholder='enter adress here' required/>"
+        childNodeHTML += "<input id='ad' type='text' name='Text' placeholder='enter adress here' required/>"
 
         node.innerHTML += childNodeHTML;
     }
+    //Change Listener
+    function init(_event: Event) {
+        let fieldset: HTMLElement = document.getElementById("fieldset")
 
-    let fieldset: HTMLElement = document.getElementById("fieldset")
-
-    fieldset.addEventListener("change", handleChange);
+        fieldset.addEventListener("change", handleChange);
+    }
 
     function handleChange(_event: Event) {
 
         let target: HTMLInputElement = <HTMLInputElement>_event.target
-        let checkoutAttribute: string= target.getAttribute("value");
-        if (target.id == "Baume") {
+        //trees
+        if (target.id == "trees") {
             let node: HTMLElement = document.getElementById("tree")
-
             let childNodeHTML: string;
-            
+
             childNodeHTML = "";
             childNodeHTML += "<a>";
-            childNodeHTML += checkoutAttribute;
+            childNodeHTML += " " + target.value;
             childNodeHTML += "</a>";
-            
-            node.innerHTML += childNodeHTML;
+
+            node.innerHTML = childNodeHTML;
         }
-        //let checkout: HTMLElement = document.getElementById("div")
+         //halterung
+        if (target.name == "Radiogroup") {
+            let node: HTMLElement = document.getElementById("holder")
+            let childNodeHTML: string;
+
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+
+            node.innerHTML = childNodeHTML;
+        }
+        //balls
+        if (target.id == "kugeln") {
+            let node: HTMLElement = document.getElementById("ball")
+
+            let childNodeHTML: string;
+
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+
+            node.innerHTML = childNodeHTML;
+        }
+        
+        if (target.id == "Menge1") {
+            let node: HTMLElement = document.getElementById("ballMenge")
+
+            let childNodeHTML: string;
+
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+
+            node.innerHTML = childNodeHTML;
+        }
+
+        //Lametta
+        if (target.id == "lametten") {
+            let node: HTMLElement = document.getElementById("lametta")
+
+            let childNodeHTML: string;
+
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+
+            node.innerHTML = childNodeHTML;
+        }
+        
+        if (target.id == "Menge2") {
+            let node: HTMLElement = document.getElementById("lamettaMenge")
+
+            let childNodeHTML: string;
+
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+
+            node.innerHTML = childNodeHTML;
+        }
+
+        //Kerzen
+        if (target.id == "candles") {
+            let node: HTMLElement = document.getElementById("candle")
+
+            let childNodeHTML: string;
+
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+
+            node.innerHTML = childNodeHTML;
+        }
+        
+        if (target.id == "Menge3") {
+            let node: HTMLElement = document.getElementById("candleMenge")
+
+            let childNodeHTML: string;
+
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+
+            node.innerHTML = childNodeHTML;
+        }
+        //Lieferoption
+        if (target.id == "ship") {
+            let node: HTMLElement = document.getElementById("shipping")
+
+            let childNodeHTML: string;
+
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+
+            node.innerHTML = childNodeHTML;
+        }
+        //Adresse
+        if (target.id == "ad") {
+            let node: HTMLElement = document.getElementById("adress")
+
+            let childNodeHTML: string;
+
+            childNodeHTML = "";
+            childNodeHTML += "<a>";
+            childNodeHTML += " " + target.value;
+            childNodeHTML += "</a>";
+
+            node.innerHTML = childNodeHTML;
+        }
 
     }
 
