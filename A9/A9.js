3409c09d60fe98ded6914461038c978519366948
Sarah <Sarah@LAPTOP-AJIP9J56.local>
2018-12-23 11:46:13 +0100

    2

--- A9/A9.js
+++ A9/A9.js
@@ -9,7 +9,13 @@ var A9Canvas;
         console.log(crc2);
         drawBezierCurve();
         drawSun();
+        drawCloud();
+        drawCloud2();
         snowflakeRandom();
+        drawSled();
+        drawSled2();
+        drawChild();
+        drawChild2();
     }
     function drawBezierCurve() {
         console.log("curve");
@@ -17,17 +23,49 @@ var A9Canvas;
         let cp1 = { x: 110, y: 500 };
         let cp2 = { x: 220, y: 410 };
         let end = { x: 360, y: 330 };
+        crc2.fillStyle = "#ffffff";
         crc2.beginPath();
         crc2.moveTo(start.x, start.y);
         crc2.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, end.x, end.y);
+        crc2.lineTo(360, 700);
+        crc2.lineTo(0, 700);
+        crc2.closePath();
         crc2.lineWidth = 2;
         crc2.stroke();
+        crc2.fill();
     }
     function drawSun() {
         console.log("sun");
         crc2.fillStyle = "#ff6000";
+        crc2.lineWidth = 1;
         crc2.beginPath();
         crc2.arc(100, 75, 50, 0, 2 * Math.PI);
+        crc2.fill();
+        crc2.stroke();
+    }
+    function drawCloud() {
+        crc2.fillStyle = "#ffffff";
+        crc2.lineWidth = 1;
+        crc2.beginPath();
+        crc2.moveTo(150, 300);
+        crc2.quadraticCurveTo(180, 250, 120, 250);
+        crc2.quadraticCurveTo(120, 200, 70, 230);
+        crc2.quadraticCurveTo(40, 250, 60, 300);
+        crc2.quadraticCurveTo(30, 350, 150, 300);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+    }
+    function drawCloud2() {
+        crc2.fillStyle = "#ffffff";
+        crc2.lineWidth = 1;
+        crc2.beginPath();
+        crc2.moveTo(300, 200);
+        crc2.quadraticCurveTo(350, 150, 270, 150);
+        crc2.quadraticCurveTo(270, 80, 220, 130);
+        crc2.quadraticCurveTo(190, 160, 210, 200);
+        crc2.quadraticCurveTo(200, 270, 300, 200);
+        crc2.closePath();
         crc2.fill();
         crc2.stroke();
     }
@@ -57,6 +95,42 @@ var A9Canvas;
         crc2.closePath();
         crc2.fill();
         crc2.stroke();
+    }
+    function drawSled() {
+        crc2.strokeStyle = "#8B4513";
+        crc2.lineWidth = 2;
+        crc2.beginPath();
+        crc2.moveTo(100, 610);
+        crc2.lineTo(130, 585);
+        crc2.moveTo(110, 603);
+        crc2.lineTo(100, 590);
+        crc2.moveTo(120, 594);
+        crc2.lineTo(111, 581);
+        crc2.moveTo(90, 598);
+        crc2.lineTo(120, 573);
+        crc2.moveTo(100, 610);
+        crc2.quadraticCurveTo(80, 630, 90, 605);
+        crc2.stroke();
+    }
+    function drawSled2() {
+        crc2.strokeStyle = "#8B4513";
+        crc2.lineWidth = 2;
+        crc2.beginPath();
+        crc2.moveTo(200, 510);
+        crc2.lineTo(230, 485);
+        crc2.moveTo(210, 503);
+        crc2.lineTo(200, 490);
+        crc2.moveTo(220, 494);
+        crc2.lineTo(211, 481);
+        crc2.moveTo(190, 498);
+        crc2.lineTo(220, 473);
+        crc2.moveTo(230, 485);
+        crc2.quadraticCurveTo(250, 470, 230, 470);
+        crc2.stroke();
+    }
+    function drawChild() {
+    }
+    function drawChild2() {
     }
 })(A9Canvas || (A9Canvas = {}));
 //# sourceMappingURL=A9.js.map
