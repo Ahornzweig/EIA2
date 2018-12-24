35918b3f9f1667a86f808ab9f0cecdd109bbd5b0
Sarah <Sarah@LAPTOP-AJIP9J56.local>
2018-12-24 15:55:39 +0100

    done

--- A9/A9.js
+++ A9/A9.js
@@ -25,14 +25,10 @@ var A9Canvas;
         drawChild2();
     }
     function drawSky() {
-        let start = { x: 0, y: 600 };
-        let cp1 = { x: 110, y: 500 };
-        let cp2 = { x: 220, y: 410 };
-        let end = { x: 360, y: 330 };
         crc2.fillStyle = "#9fc1f9";
         crc2.beginPath();
-        crc2.moveTo(start.x, start.y);
-        crc2.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, end.x, end.y);
+        crc2.moveTo(0, 600);
+        crc2.bezierCurveTo(110, 500, 220, 410, 360, 330);
         crc2.lineTo(360, 0);
         crc2.lineTo(0, 0);
         crc2.closePath();
@@ -42,14 +38,11 @@ var A9Canvas;
     }
     function drawHill() {
         console.log("curve");
-        let start = { x: 0, y: 600 };
-        let cp1 = { x: 110, y: 500 };
-        let cp2 = { x: 220, y: 410 };
-        let end = { x: 360, y: 330 };
         crc2.fillStyle = "#ffffff";
+        crc2.strokeStyle = "#9999ff";
         crc2.beginPath();
-        crc2.moveTo(start.x, start.y);
-        crc2.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, end.x, end.y);
+        crc2.moveTo(0, 600);
+        crc2.bezierCurveTo(110, 500, 220, 410, 360, 330);
         crc2.lineTo(360, 700);
         crc2.lineTo(0, 700);
         crc2.closePath();
@@ -60,16 +53,13 @@ var A9Canvas;
     function drawSun() {
         console.log("sun");
         crc2.fillStyle = "#f46542";
-        crc2.lineWidth = 1;
         crc2.beginPath();
         crc2.arc(100, 75, 50, 0, 2 * Math.PI);
         crc2.fill();
-        crc2.stroke();
     }
     function drawCloud() {
         console.log("cloud1");
         crc2.fillStyle = "#ffffff";
-        crc2.lineWidth = 1;
         crc2.beginPath();
         crc2.moveTo(150, 300);
         crc2.quadraticCurveTo(180, 250, 120, 250);
@@ -78,12 +68,10 @@ var A9Canvas;
         crc2.quadraticCurveTo(30, 350, 150, 300);
         crc2.closePath();
         crc2.fill();
-        crc2.stroke();
     }
     function drawCloud2() {
         console.log("cloud2");
         crc2.fillStyle = "#ffffff";
-        crc2.lineWidth = 1;
         crc2.beginPath();
         crc2.moveTo(300, 200);
         crc2.quadraticCurveTo(350, 150, 270, 150);
@@ -92,10 +80,9 @@ var A9Canvas;
         crc2.quadraticCurveTo(200, 270, 300, 200);
         crc2.closePath();
         crc2.fill();
-        crc2.stroke();
     }
     function snowflakeRandom() {
-        console.log("hiJan");
+        console.log("randomJan");
         for (let i = 0; i < 50; i++) {
             let x = Math.random() * crc2.canvas.width;
             let y = Math.random() * crc2.canvas.height;
@@ -122,8 +109,8 @@ var A9Canvas;
         crc2.stroke();
     }
     function drawSled() {
-        console.log("hiNico");
         for (let i = 0; i < 2; i++) {
+            console.log("Nico" + i);
             crc2.strokeStyle = "#8B4513";
             crc2.lineWidth = 2;
             crc2.beginPath();
@@ -141,7 +128,7 @@ var A9Canvas;
         }
     }
     function drawSled2() {
-        //console.log("hiJulia");
+        console.log("Julia");
         crc2.strokeStyle = "#8B4513";
         crc2.lineWidth = 2;
         crc2.beginPath();
@@ -155,6 +142,55 @@ var A9Canvas;
         crc2.lineTo(220, 473);
         crc2.moveTo(230, 485);
         crc2.quadraticCurveTo(250, 470, 230, 470);
+        crc2.stroke();
+    }
+    function treeRandom() {
+        for (let i = 0; i < 7; i++) {
+            let x = Math.random() * crc2.canvas.width;
+            let y = Math.random() * crc2.canvas.height;
+            crc2.beginPath();
+            crc2.moveTo(0, 600);
+            crc2.bezierCurveTo(110, 500, 220, 410, 360, 330);
+            if (crc2.isPointInPath(x, y)) {
+                drawTrees(x, y);
+            }
+            else {
+                i--;
+            }
+        }
+    }
+    function drawTrees(_x, _y) {
+        console.log("Trees", _x, _y);
+        crc2.fillStyle = "#A52A2A";
+        crc2.strokeStyle = "#A52A2A";
+        crc2.lineWidth = 1;
+        crc2.beginPath();
+        crc2.moveTo(_x + 5, _y + 7.5);
+        crc2.lineTo(_x - 5, _y + 7.5);
+        crc2.lineTo(_x - 5, _y - 12.5);
+        crc2.lineTo(_x + 5, _y - 12.5);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+        crc2.fillStyle = "#228B22";
+        crc2.strokeStyle = "#0f7027";
+        crc2.lineWidth = 1;
+        crc2.beginPath();
+        crc2.moveTo(_x - 30, _y - 10);
+        crc2.lineTo(_x, _y - 55);
+        crc2.lineTo(_x + 30, _y - 10);
+        crc2.closePath();
+        crc2.fill();
+        crc2.stroke();
+        crc2.fillStyle = "#228B22";
+        crc2.strokeStyle = "#0f7027";
+        crc2.lineWidth = 1;
+        crc2.beginPath();
+        crc2.moveTo(_x - 27, _y - 27.5);
+        crc2.lineTo(_x, _y - 75);
+        crc2.lineTo(_x + 27, _y - 27.5);
+        crc2.closePath();
+        crc2.fill();
         crc2.stroke();
     }
     function drawChild() {
@@ -203,59 +239,6 @@ var A9Canvas;
         crc2.beginPath();
         crc2.moveTo(252, 430);
         crc2.arc(252, 422, 6, 0, 2 * Math.PI);
-        crc2.fill();
-        crc2.stroke();
-    }
-    function treeRandom() {
-        for (let i = 0; i < 7; i++) {
-            let x = Math.random() * crc2.canvas.width;
-            let y = Math.random() * crc2.canvas.height;
-            let start = { x: 0, y: 600 };
-            let cp1 = { x: 110, y: 500 };
-            let cp2 = { x: 220, y: 410 };
-            let end = { x: 360, y: 330 };
-            crc2.beginPath();
-            crc2.moveTo(start.x, start.y);
-            crc2.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, end.x, end.y);
-            if (crc2.isPointInPath(x, y)) {
-                drawTrees(x, y);
-            }
-            else {
-                i--;
-            }
-        }
-    }
-    function drawTrees(_x, _y) {
-        console.log("Trees", _x, _y);
-        crc2.fillStyle = "#A52A2A";
-        crc2.strokeStyle = "#000000";
-        crc2.lineWidth = 1;
-        crc2.beginPath();
-        crc2.moveTo(_x + 5, _y + 7.5);
-        crc2.lineTo(_x - 5, _y + 7.5);
-        crc2.lineTo(_x - 5, _y - 12.5);
-        crc2.lineTo(_x + 5, _y - 12.5);
-        crc2.closePath();
-        crc2.fill();
-        crc2.stroke();
-        crc2.fillStyle = "#228B22";
-        crc2.strokeStyle = "#000000";
-        crc2.lineWidth = 1;
-        crc2.beginPath();
-        crc2.moveTo(_x - 20, _y - 13);
-        crc2.lineTo(_x, _y - 55);
-        crc2.lineTo(_x + 20, _y - 13);
-        crc2.closePath();
-        crc2.fill();
-        crc2.stroke();
-        crc2.fillStyle = "#228B22";
-        crc2.strokeStyle = "#000000";
-        crc2.lineWidth = 1;
-        crc2.beginPath();
-        crc2.moveTo(_x - 20, _y - 23);
-        crc2.lineTo(_x, _y - 65);
-        crc2.lineTo(_x + 20, _y - 23);
-        crc2.closePath();
         crc2.fill();
         crc2.stroke();
     }
