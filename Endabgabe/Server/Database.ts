6668aa16aa198feb8dcbb1005d0d7462efc0e3ca
Sarah <Sarah@LAPTOP-AJIP9J56.eduroam.hs-furtwangen.de>
2019-02-13 15:19:37 +0100

    2

--- Endabgabe/Server/Database.ts
+++ Endabgabe/Server/Database.ts
@@ -27,7 +27,7 @@ function handleConnect(_e: Mongo.MongoError, _db: Mongo.Db): void {
     else {
         console.log("Connected to database!");
         db = _db.db(databaseName);
-        students = db.collection("students");
+        students = db.collection("RH");
     }
 }
 
